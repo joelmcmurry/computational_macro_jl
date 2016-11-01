4940689c706906f1805ca0dbb6709de208bce9e6
joelmcmurry <joel.mcmurry@gmail.com>
2016-11-01 07:33:50 -0500

    update primitive constructor

--- conesa_krueger_model.jl
+++ conesa_krueger_model.jl
@@ -9,6 +9,7 @@ using QuantEcon: gridmake
 
 type Primitives
   N :: Int64 ##  number of periods agent is alive
+  JR :: Int64 ## retirement age
   n :: Float64 ## population growth rate
   beta :: Float64 ## discount rate
   gamma :: Float64 ## utility consumption weight (relative to leisure)
@@ -18,7 +19,7 @@ type Primitives
   w :: Float64 ## wage rate
   r :: Float64 ## interest rate
   b :: Float64 ## pension benefit
-  q :: Float64 ## discount bond price
+  theta :: Float64 ## social security tax
   a_min :: Float64 ## minimum asset value
   a_max :: Float64 ## maximum asset value
   a_size :: Int64 ## size of asset grid
@@ -31,4 +32,30 @@ type Primitives
   a_z_vals :: Array{Float64} ## array of states: (a,z) combinations
   a_z_indices :: Array{Int64} ## indices of states: (a,z) combinations
   M :: Int64 ## number of possible (a,z) combination
+end
+
+#= Outer Constructor for Primitives. Supplies default values, field
+names, and creates grid objects=#
+
+function Primitives(;N::Int64=66,JR::Int64=46,n::Float64=0.011,beta::Float64=0.97,
+  gamma::Float64=0.42,sigma::Float64=2,delta::Float64=0.06,alpha::Float64=0.36,
+  w::Float64=1.05,r::Float64=0.05,b::Float64=0.2,theta::Float64=0.11,a_min::Float64=0.0,
+  a_max::Float64=5.0,a_size::Int64=100,z_vals=[3.0;0.5],
+  z_markov=[0.9261 (1-0.9261);(1-0.9811) 0.9811],z_ergodic=[0.2037 1-0.2037])
+
+  # Grids
+
+  a_vals = linspace(a_min, a_max, a_size)
+  z_size = length(markov[:,1])
+  a_indices = gridmake(1:a_size)
+  M = a_size*z_size
+  a_z_vals = gridmake(a_vals,z_vals)
+  a_z_indices = gridmake(1:a_size,1:z_size)
+
+  primitives = Primitives(N, JR, n, beta, gamma, sigma, delta, alpha, w, r, b,
+    theta, a_min, a_max, a_size, a_vals, z_size, z_vals, z_markov, z_ergodic, a_z_vals,
+    a_z_indices, M)
+
+  return primitives
+
 end
