6cec8683ae9666acbec07371bbd6d84ced7ba81c
joelmcmurry <joel.mcmurry@gmail.com>
2016-11-30 20:27:14 -0600

    vfi

--- krusell_smith_model.jl
+++ krusell_smith_model.jl
@@ -59,8 +59,8 @@ end
 function Primitives(;beta::Float64=0.99, alpha::Float64=0.36, delta::Float64=0.025,
  z::Array{Float64}=[1.01 0.99],epsilon::Array{Int64}=[0  1],
  u::Array{Float64}=[0.04 0.10], ebar::Float64=0.3271,
- k_min::Float64=0.00, k_max::Float64=15.0, k_size::Int64=100,
- K_min::Float64=1.00e-10, K_max::Float64=15.0, K_size::Int64=100,
+ k_min::Float64=1.00e-2, k_max::Float64=15.0, k_size::Int64=100,
+ K_min::Float64=1.00e-2, K_max::Float64=15.0, K_size::Int64=10,
  N::Int64=5000,T::Int64=11000,a0=0.095,b0=0.085,a1=0.999,b1=0.999)
 
   # Grids
@@ -84,6 +84,15 @@ function Primitives(;beta::Float64=0.99, alpha::Float64=0.36, delta::Float64=0.0
 
   w = [(1-alpha)*z[1]*(K_vals./L[1]).^alpha (1-alpha)*z[2]*(K_vals./L[2]).^alpha]
   r = [alpha*z[1]*(K_vals./L[1]).^(alpha-1) alpha*z[2]*(K_vals./L[2]).^(alpha-1)]
+
+  # correct prices for K=0
+
+  for K_index in 1:K_size
+    if K_vals[K_index] == 0.0
+      w[K_index,:] = [(1-alpha)*z[1]*(1e-10/L[1])^alpha (1-alpha)*z[2]*(1e-10/L[2])^alpha]
+      r[K_index,:] = [alpha*z[1]*(1e-10/L[1])^(alpha-1) alpha*z[2]*(1e-10/L[2])^(alpha-1)]
+    end
+  end
 
   # Initial guess for K
 
@@ -114,10 +123,10 @@ type Results
     Tvg1::Array{Float64}
     Tvb1::Array{Float64}
     num_iter::Int
-    sigmag0::Array{Int}
-    sigmab0::Array{Int}
-    sigmag1::Array{Int}
-    sigmab1::Array{Int}
+    sigmag0::Array
+    sigmab0::Array
+    sigmag1::Array
+    sigmab1::Array
 
     function Results(prim::Primitives)
 
@@ -169,18 +178,20 @@ end
 
 ## Bellman Operator
 
-function bellman_operator!(prim::Primitives,
-  vg0::Array{Float64},vb0::Array{Float64},
-  vg1::Array{Float64},vb1::Array{Float64})
+# Grid search over k
+
+function bellman_operator_grid!(prim::Primitives,
+  vg1::Array{Float64},vb1::Array{Float64},
+  vg0::Array{Float64},vb0::Array{Float64})
   # initialize
-  Tvg0 = fill(-Inf,(prim.k_size,prim.K_size))
-  Tvb0 = fill(-Inf,(prim.k_size,prim.K_size))
-  Tvg1 = fill(-Inf,(prim.k_size,prim.K_size))
-  Tvb1 = fill(-Inf,(prim.k_size,prim.K_size))
-  sigmag0 = zeros(prim.k_size,prim.K_size)
-  sigmab0 = zeros(prim.k_size,prim.K_size)
+  Tvg1 = fill(-Inf,prim.k_size,prim.K_size)
+  Tvb1 = fill(-Inf,prim.k_size,prim.K_size)
+  Tvg0 = fill(-Inf,prim.k_size,prim.K_size)
+  Tvb0 = fill(-Inf,prim.k_size,prim.K_size)
   sigmag1 = zeros(prim.k_size,prim.K_size)
   sigmab1 = zeros(prim.k_size,prim.K_size)
+  sigmag0 = zeros(prim.k_size,prim.K_size)
+  sigmab0 = zeros(prim.k_size,prim.K_size)
 
   # interpolate value functions
   itp_vg0 = interpolate(vg0,BSpline(Cubic(Line())),OnGrid())
@@ -191,73 +202,46 @@ function bellman_operator!(prim::Primitives,
   # find max value for each (k,K,epsilon,z) combination
   for z_index in 1:prim.z_size
     for eps_index in 1:prim.epsilon_size
+      # initialize floating Tv and sigma
+      # Tv = ones(Float64,prim.k_size,prim.K_size)*(-2^51)
+      Tv = fill(-Inf,prim.k_size,prim.K_size)
+      sigma = zeros(prim.k_size,prim.K_size)
+
       # find row index of shocks for transition matrix
-      if z_index == 1 & eps_index == 2
+      if z_index == 1 && eps_index == 2
         shock_index = 1
-      elseif z_index == 2 & eps_index == 2
+      elseif z_index == 2 && eps_index == 2
         shock_index = 2
-      elseif z_index == 1 & eps_index == 1
+      elseif z_index == 1 && eps_index == 1
         shock_index = 3
-      elseif z_index == 2 & eps_index == 1
+      elseif z_index == 2 && eps_index == 1
         shock_index = 4
       end
 
       for K_index in 1:prim.K_size
         # approximate aggregate capital tomorrow (log linear form)
-        if prim.K_vals[K_index] == 0.0
-          if z_index == 1
-            Kprime = exp(prim.a0 + prim.a1*log(1e-10))
-          else
-            Kprime = exp(prim.b0 + prim.b1*log(1e-10))
-          end
+        if z_index == 1
+          Kprime = exp(prim.a0 + prim.a1*log(prim.K_vals[K_index]))
         else
-          if z_index == 1
-            Kprime = exp(prim.a0 + prim.a1*log(prim.K_vals[K_index]))
-          else
-            Kprime = exp(prim.b0 + prim.b1*log(prim.K_vals[K_index]))
-          end
+          Kprime = exp(prim.b0 + prim.b1*log(prim.K_vals[K_index]))
         end
+
 
         # find index of aggregate capital tomorrow
         findmatch(Kprime_index)=abs(prim.itp_K[Kprime_index]-Kprime)
         Kprime_index = optimize(Kprime_index->findmatch(Kprime_index),1.0,prim.K_size).minimum
 
-        # Option for bilinear interpolation
-
-        # for k_index in 1:prim.k_size
-        #   objective(kprime_index) = -log(prim.r[K_index]*prim.k_vals[k_index] +
-        #     prim.w[K_index]*prim.epsilon[eps_index] + (1-prim.delta)*prim.k_vals[k_index]
-        #     - itp_k[kprime_index]) -
-        #     prim.beta*
-        #     (prim.transmat[shock_index,1]*itp_vg1[kprime_index,Kprime_index]+
-        #     prim.transmat[shock_index,2]*itp_vb1[kprime_index,Kprime_index]+
-        #     prim.transmat[shock_index,3]*itp_vg0[kprime_index,Kprime_index]+
-        #     prim.transmat[shock_index,4]*itp_vb0[kprime_index,Kprime_index])
-        #   lower = 1.0
-        #   upper = findlast(kprime_index->kprime_index<prim.r[K_index]*prim.k_vals[k_index] +
-        #     prim.w[K_index]*prim.epsilon[eps_index] + (1-prim.delta)*prim.k_vals[k_index]
-        #     ,prim.itp_k)
-        #   opt_choice = optimize(kprime_index->objective(kprime_index),lower,upper)
-        #
-        #   Tv[k_index,K_index] = -objective(opt_choice.minimum)
-        #   sigma[k_index,K_index] = itp_k[opt_choice.minimum]
-        # end
-
-        # Option for grid search over k
-
         kprime_lower = 1 # initialize lower bound of asset choices
         for k_index in 1:prim.k_size
-          k = prim.k_vals[k_index]
 
           max_value = -Inf # initialize value for combinations
 
             for kprime_index in kprime_lower:prim.k_size
-              kprime = prim.k_vals[kprime_index]
-              c = prim.r[K_index]*prim.k_vals[k_index] +
-                prim.w[K_index]*prim.epsilon[eps_index] +
-                (1-prim.delta)*prim.k_vals[k_index] - kprime
+              c = prim.r[K_index,z_index]*prim.k_vals[k_index] +
+                prim.w[K_index,z_index]*prim.epsilon[eps_index] +
+                (1-prim.delta)*prim.k_vals[k_index] - prim.k_vals[kprime_index]
               if c > 0
-                value = log(c) -
+                value = log(c) +
                     prim.beta*
                     (prim.transmat[shock_index,1]*itp_vg1[kprime_index,Kprime_index]+
                     prim.transmat[shock_index,2]*itp_vb1[kprime_index,Kprime_index]+
@@ -272,39 +256,181 @@ function bellman_operator!(prim::Primitives,
             end
           Tv[k_index,K_index] = max_value
         end
+      end
 
+      # fill in proper Tv and sigma
+      if z_index == 1 && eps_index == 2
+        Tvg1 = Tv
+        sigmag1 = sigma
+      elseif z_index == 2 && eps_index == 2
+        Tvb1 = Tv
+        sigmab1 = sigma
+      elseif z_index == 1 && eps_index == 1
+        Tvg0 = Tv
+        sigmag0 = sigma
+      elseif z_index == 2 && eps_index == 1
+        Tvb0 = Tv
+        sigmab0 = sigma
       end
     end
   end
-  Tv, sigma
+  Tvg1, Tvb1, Tvg0, Tvb0, sigmag1, sigmab1, sigmag0, sigmab0
 end
+
+# Optimize over linear interpolated k
+
+function bellman_operator_itp!(prim::Primitives,
+  vg1::Array{Float64},vb1::Array{Float64},
+  vg0::Array{Float64},vb0::Array{Float64})
+  # initialize
+  Tvg1 = fill(-Inf,prim.k_size,prim.K_size)
+  Tvb1 = fill(-Inf,prim.k_size,prim.K_size)
+  Tvg0 = fill(-Inf,prim.k_size,prim.K_size)
+  Tvb0 = fill(-Inf,prim.k_size,prim.K_size)
+  sigmag1 = zeros(Float64,prim.k_size,prim.K_size)
+  sigmab1 = zeros(Float64,prim.k_size,prim.K_size)
+  sigmag0 = zeros(Float64,prim.k_size,prim.K_size)
+  sigmab0 = zeros(Float64,prim.k_size,prim.K_size)
+
+  # interpolate value functions
+  itp_vg0 = interpolate(vg0,BSpline(Cubic(Line())),OnGrid())
+  itp_vb0 = interpolate(vb0,BSpline(Cubic(Line())),OnGrid())
+  itp_vg1 = interpolate(vg1,BSpline(Cubic(Line())),OnGrid())
+  itp_vb1 = interpolate(vb1,BSpline(Cubic(Line())),OnGrid())
+
+  # find max value for each (k,K,epsilon,z) combination
+  for z_index in 1:prim.z_size
+    for eps_index in 1:prim.epsilon_size
+      # initialize floating Tv and sigma
+      # Tv = ones(Float64,prim.k_size,prim.K_size)*(-2^51)
+      Tv = fill(-Inf,prim.k_size,prim.K_size)
+      sigma = zeros(Float64,prim.k_size,prim.K_size)
+
+      # find row index of shocks for transition matrix
+      if z_index == 1 && eps_index == 2
+        shock_index = 1
+      elseif z_index == 2 && eps_index == 2
+        shock_index = 2
+      elseif z_index == 1 && eps_index == 1
+        shock_index = 3
+      elseif z_index == 2 && eps_index == 1
+        shock_index = 4
+      end
+
+      for K_index in 1:prim.K_size
+        # approximate aggregate capital tomorrow (log linear form)
+        if z_index == 1
+          Kprime = exp(prim.a0 + prim.a1*log(prim.K_vals[K_index]))
+        else
+          Kprime = exp(prim.b0 + prim.b1*log(prim.K_vals[K_index]))
+        end
+
+
+        # find index of aggregate capital tomorrow
+        findmatch(Kprime_index)=abs(prim.itp_K[Kprime_index]-Kprime)
+        Kprime_index = optimize(Kprime_index->findmatch(Kprime_index),1.0,prim.K_size).minimum
+
+        for k_index in 1:prim.k_size
+          if prim.r[K_index]*prim.k_vals[k_index] +
+            prim.w[K_index]*prim.epsilon[eps_index] +
+            (1-prim.delta)*prim.k_vals[k_index] > 0
+
+            objective(kprime_index) = -log(prim.r[K_index]*prim.k_vals[k_index] +
+              prim.w[K_index]*prim.epsilon[eps_index] + (1-prim.delta)*prim.k_vals[k_index]
+              - prim.itp_k[kprime_index]) -
+              prim.beta*
+              (prim.transmat[shock_index,1]*itp_vg1[kprime_index,Kprime_index]+
+              prim.transmat[shock_index,2]*itp_vb1[kprime_index,Kprime_index]+
+              prim.transmat[shock_index,3]*itp_vg0[kprime_index,Kprime_index]+
+              prim.transmat[shock_index,4]*itp_vb0[kprime_index,Kprime_index])
+            lower = 1.0
+            upper = findlast(kprime_index->kprime_index<=prim.r[K_index]*prim.k_vals[k_index] +
+              prim.w[K_index]*prim.epsilon[eps_index] + (1-prim.delta)*prim.k_vals[k_index]
+              ,prim.itp_k)
+            if lower < upper
+              optimization = optimize(kprime_index->objective(kprime_index),lower,upper)
+              optimizer = optimization.minimum
+            else
+              optimizer = lower
+            end
+
+            Tv[k_index,K_index] = -objective(optimizer)
+            sigma[k_index,K_index] = prim.itp_k[optimizer]
+          end
+        end
+      end
+
+      # fill in proper Tv and sigma
+      if z_index == 1 && eps_index == 2
+        Tvg1 = Tv
+        sigmag1 = sigma
+      elseif z_index == 2 && eps_index == 2
+        Tvb1 = Tv
+        sigmab1 = sigma
+      elseif z_index == 1 && eps_index == 1
+        Tvg0 = Tv
+        sigmag0 = sigma
+      elseif z_index == 2 && eps_index == 1
+        Tvb0 = Tv
+        sigmab0 = sigma
+      end
+    end
+  end
+  Tvg1, Tvb1, Tvg0, Tvb0, sigmag1, sigmab1, sigmag0, sigmab0
+end
+
+########################SPEED TEST#######################
+
+tic()
+res=Results(prim)
+vfi!(bellman_operator_grid!,prim,res,1000,1e-1)
+toc()
+
+tic()
+res=Results(prim)
+vfi!(bellman_operator_itp!,prim,res,1000,1e-1)
+toc()
+
+#########################################################
 
 ## Value Function Iteration
 
-# function vfi!(prim::Primitives, res::Results,
-#   max_iter::Integer, epsilon::Real)
-#     if prim.beta == 0.0
-#         tol = Inf
-#     else
-#         tol = epsilon
-#     end
-#
-#     for i in 1:max_iter
-#         # updates Tv and sigma in place
-#         res.Tv, res.sigma = bellman_operator!(prim,res.v)
-#
-#         # compute error and update the value with Tv inside results
-#         err = maxabs(res.Tv .- res.v)
-#         copy!(res.v, res.Tv)
-#         res.num_iter += 1
-#
-#         if err < tol
-#             break
-#         end
-#     end
-#
-#     res
-# end
+function vfi!(T::Function, prim::Primitives, res::Results,
+  max_iter::Integer, epsilon::Real)
+    if prim.beta == 0.0
+        tol = Inf
+    else
+        tol = epsilon
+    end
+
+    for i in 1:max_iter
+        # updates Tv and sigma in place
+        res.Tvg1, res.Tvb1, res.Tvg0, res.Tvb0,
+          res.sigmag1, res.sigmab1, res.sigmag0, res.sigmab0 =
+          T(prim,res.vg1,res.vb1,res.vg0,res.vb0)
+
+        # compute error and update the value with Tv inside results
+        errg1 = maxabs(res.Tvg1 .- res.vg1)
+        errg0 = maxabs(res.Tvg0 .- res.vg0)
+        errb1 = maxabs(res.Tvb1 .- res.vb1)
+        errb0 = maxabs(res.Tvb0 .- res.vb0)
+        err = max(errg1,errg0,errb1,errb0)
+
+        println("Iter: ", res.num_iter, " error: ", err)
+
+        res.vg1 = res.Tvg1
+        res.vg0 = res.Tvg0
+        res.vb1 = res.Tvb1
+        res.vb0 = res.Tvb0
+        res.num_iter += 1
+
+        if err < tol
+            break
+        end
+    end
+
+    res
+end
 
 ## Create transmition matrix
 
